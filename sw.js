4b983db15b305c4734b5ef4ed3746333f639d6c5
yukiya-web <zhongcungongye5@gmail.com>
2025-07-15 13:32:11 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installing.');
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activating.');
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return new Response('ネットワークに接続できません。');
+    })
+  );
+});
